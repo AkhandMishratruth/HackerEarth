560fd0e3643e7316e6136b9d24a16f0d53a358b9
Akhand <apratap.mishra.cer15@itbhu.ac.in>
2017-06-28 19:07:41 +0530

    2 more

--- Dynamic Programming 1/GudiAndTheMagicalOrbs.java
+++ Dynamic Programming 1/GudiAndTheMagicalOrbs.java
@@ -1,22 +1,47 @@
 import java.io.*;
 import java.util.*;
 
-public class Gudi_and_the_Magical_Orbs {
-    static int[][] ar;
-    static int[][][] dp;
+public class GudiAndTheMagicalOrbs {
     public static void main(String args[]) throws Exception {
         FastReader in = new FastReader();
+        int[][] ar;
+        int[][][] dp;
         int t = in.nextInt(), r, c, orb;
         while(t-->0){
             r=in.nextInt();c=in.nextInt();orb=in.nextInt();
             ar=new int[r][c];
             dp= new int[r][c][orb+1];
+            int ans;
             for(int i=0;i<ar.length;i++)
                 for(int j=0;j<ar[0].length;j++)
                     ar[i][j]=in.nextInt();
-            System.out.println(solve(r-1,c-1,orb));
+
+            for(int i=0;i<dp.length;i++){
+                for(int j=0;j<dp[0].length;j++){
+                    for(int k=1;k<dp[0][0].length;k++){
+                        if(i==0 && j==0 && k>=ar[0][0])
+                            dp[i][j][k]=ar[0][0];
+                        else if(k>=ar[i][j]){
+                            ans=-1;
+                            if(i>0)
+                                ans=Math.max(ans,dp[i-1][j][k-ar[i][j]]);
+                            if(j>0)
+                                ans=Math.max(ans,dp[i][j-1][k-ar[i][j]]);
+                            if(j>0&&i>0)
+                                ans=Math.max(ans,dp[i-1][j-1][k-ar[i][j]]);
+                            if(ans<=0)
+                                dp[i][j][k]=-1;
+                            else
+                                dp[i][j][k]=ans+ar[i][j];
+                        }
+                        else
+                            dp[i][j][k]=-1;
+                    }
+                }
+            }
+            System.out.println(dp[r-1][c-1][orb]);
         }
-    }
+    }/*
     static int solve(int r,int c, int k){
         if(r==0 && c==0){
             if(k>=ar[0][0])
@@ -44,7 +69,8 @@ public class Gudi_and_the_Magical_Orbs {
                 dp[r][c][k]=ans+ar[r][c];
             return dp[r][c][k];
         }
-    }
+
+    }*/
     static class FastReader {
         BufferedReader br;
         StringTokenizer st;
