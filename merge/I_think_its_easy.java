3fb8c7c7e6ec58f672774c9b9e9ca472db7c27f7
AkhandMishratruth <apratap.mishra.cer15@it.bhu.ac.in>
2018-05-21 23:28:05 +0530

    merge

---
+++ merge/I_think_its_easy.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class  I_think_its_easy {
+    public static void main(String args[] ) throws Exception {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+    }
+}
+
+class IndVal implements  {
+    int ind, val;
+
+    public IndVal(int ind, int val) {
+        this.ind = ind;
+        this.val = val;
+    }
+
+}
